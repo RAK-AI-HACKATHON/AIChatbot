d1c9c280600f236b7c2f54f348fb34d61e2e3ccf
RAK-AI-HACKATHON <127503078+RAK-AI-HACKATHON@users.noreply.github.com>
2023-03-15 15:01:24 +0400

    Update ChequeBookService.java

--- src/main/java/com/rak/chatbot/service/ChequeBookService.java
+++ src/main/java/com/rak/chatbot/service/ChequeBookService.java
@@ -15,10 +15,18 @@ public class ChequeBookService {
 	ChequeBookRepository chequeBookRepository;
 
 	public String checkStatus(String chequeBookReqNo) {
+		String status = null;
 		Optional<ChequeBook> chequeBookReq = chequeBookRepository.findByRequestNumber(chequeBookReqNo);
 		if(chequeBookReq.isPresent()) {
-			String status = chequeBookReq.get().getStatus();
+			 status = chequeBookReq.get().getStatus();
 			return "Cheque Book status is " + status;
+		}else { //If requesNumber search fails, searching with Account number, as input we got from User might be an account Number 
+			String accountNumber = chequeBookReqNo;
+			Optional<ChequeBook> chequeBookReqbyAcctNumber = chequeBookRepository.findByAccountNumber(accountNumber);
+			if(chequeBookReqbyAcctNumber.isPresent()) {
+				 status = chequeBookReqbyAcctNumber.get().getStatus();
+				 return "Cheque Book status is " + status;
+			}
 		}
 		return "Invalid Cheque Book Request Number. Kindly enter the valid number.";
 	}
